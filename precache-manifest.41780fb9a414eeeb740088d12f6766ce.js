b74a22e05a7251af05217ff7b398ce212bbb3c54
Mirjana Vukojevic <mirjana.vukojevic@quantox.com>
2019-05-22 14:36:43 +0200

    Updates

---
+++ precache-manifest.41780fb9a414eeeb740088d12f6766ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0b03fd6a42fb090d7e41f27e4c04d0",
+    "url": "/miravukojevic/react-trivia/index.html"
+  },
+  {
+    "revision": "d4a5595d80e960c764fc",
+    "url": "/miravukojevic/react-trivia/static/css/main.a5f60f65.chunk.css"
+  },
+  {
+    "revision": "82163594d945de5a9693",
+    "url": "/miravukojevic/react-trivia/static/js/2.b980ab02.chunk.js"
+  },
+  {
+    "revision": "d4a5595d80e960c764fc",
+    "url": "/miravukojevic/react-trivia/static/js/main.c43d03d0.chunk.js"
+  },
+  {
+    "revision": "763c9aeeb387c8c9766b",
+    "url": "/miravukojevic/react-trivia/static/js/runtime~main.d23cf76e.js"
+  },
+  {
+    "revision": "7328a5f0b40af00a8940effa34a1aa39",
+    "url": "/miravukojevic/react-trivia/static/media/brainstorm.7328a5f0.png"
+  }
+]);
